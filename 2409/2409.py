6883b618e59fec6c2b967498c38ea68ab55f7f6e
leoharchilava <91017533+leoharchilava@users.noreply.github.com>
2021-10-03 22:52:16 +0300

    2409 hw

---
+++ 2409/2409.py
@@ -0,0 +1,11 @@
+f = open('C:\Users\LEONARD\Desktop/240921','r')
+d = f.readlines()
+print([d])
+f.close()
+
+
+
+f = open('C:\Users\LEONARD\Desktop/240921','r')
+p = f.writelines()
+print([p])
+f.close()
